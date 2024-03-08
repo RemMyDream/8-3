6f91df94e595b1e321d27600080c75b3110ca578
KChien <154885346+RemMyDream@users.noreply.github.com>
2024-03-08 14:36:55 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào Hằng, Chiến có điều nho nhỏ muốn nói. </br> Nhân dịp 8/3 này, tớ chúc cậu mãi luôn mỉm cười và vui vẻ, bên cạnh đó tớ muốn tỏ tình lại với cậu nữa: Làm người yêu tớ nhé!",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
   titleModar: "Cảm ơn cậu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn cậu đã tiếp tục đồng hành cùng tớ sau một khoảng thời gian dài và tớ mong mình có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ chở cậu đi ăn bằng cách đặt đồ ăn ngon cho cậu!. Moazzzz.</br> Love you <3",
 };
